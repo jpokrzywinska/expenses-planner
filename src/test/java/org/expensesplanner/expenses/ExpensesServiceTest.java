00bc12e84b606204f2f1e1ff03ef4ae7b372e301
Justyna Pokrzywinska <justyna.pokrzywinska@capgemini.com>
2018-11-21 22:27:59 +0100

    added tests, update gitignore

--- src/test/java/org/expensesplanner/expenses/ExpensesServiceTest.java
+++ src/test/java/org/expensesplanner/expenses/ExpensesServiceTest.java
@@ -1,24 +1,59 @@
 package org.expensesplanner.expenses;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class ExpensesServiceTest {
 
     private ExpensesService sut;
+    private ExpensesRepository expensesRepo;
+
+
+    @Before
+    public void setUp(){
+        sut = new ExpensesService(new ExpenseValidator());
+    }
 
     @Test
     public void addExpense(){
         //given
         ExpenseDto expenseDto = new ExpenseDto();
+        ArgumentCaptor<ExpenseEntity> entityCaptor = ArgumentCaptor.forClass(ExpenseEntity.class);
+
+        //when
+        sut.add(expenseDto);
+
+        //then
+        verify(expensesRepo).add(entityCaptor.capture());
+        ExpenseEntity result = entityCaptor.getValue();
+        assertThat(result.getName()).isEqualTo(expenseDto.getName());
+        assertThat(result.getPrice()).isEqualTo(expenseDto.getPrice());
+        assertThat(result.getDate()).isEqualTo(expenseDto.getDate());
+        assertThat(result.getPerson()).isEqualTo(expenseDto.getPerson());
+        assertThat(result.getCategory().getName()).isEqualTo(expenseDto.getCategory().getName());
+
+    }
+
+    @Test(expected = InvalidExpenseException.class)
+    public void shouldThrowExceptionOnValidationErrorWhenNameIsEmpty(){
+        //given
+        ExpenseDto expenseDto = new ExpenseDto();
+        expenseDto.setName(" ");
 
         //when
         sut.add(expenseDto);
     }
 
     @Test(expected = InvalidExpenseException.class)
-    public void shouldThrowExceptionOnValidationError(){
+    public void shouldThrowExceptionOnValidationErrorWhenPriceIsEmpty(){
         //given
         ExpenseDto expenseDto = new ExpenseDto();
+        expenseDto.setPrice(null);
 
         //when
         sut.add(expenseDto);
